dde2b686026d175e23ec3224b307b22ee75d81f7
kelson-medeiros-at-lais <33149224+kelson-medeiros-at-lais@users.noreply.github.com>
2024-09-24 10:52:54 -0300

    env: [add] avasus3

---
+++ workspaces/avasus3.sh
@@ -0,0 +1,19 @@
+PROJECT_ROOTDIR=~/projetos/LAIS/avasus3
+WORKSPACE_ROOTDIR=~/projetos/LAIS/avasus3
+
+# Baixe o projeto na pasta de exemplo (se for outra, basta que altere os scripts)
+mkdir -p $PROJECT_ROOTDIR
+git clone ssh://git@git.lais.huol.ufrn.br:2222/AVAS/avasus/avasus3/avasus3_workspace.git $WORKSPACE_ROOTDIR
+cd $WORKSPACE_ROOTDIR
+
+
+./avasus3 setup
+
+
+if [[ "$SHELL" == '/bin/bash' ]]; then
+    source ~/.bashrc
+fi
+
+if [[ "$SHELL" == '/bin/zsh' ]]; then
+    source ~/.zshrc
+fi
